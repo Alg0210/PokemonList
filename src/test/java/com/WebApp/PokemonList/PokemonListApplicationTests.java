5232ea954dab3e48bb6cb2a4ec507aaef16d160b
Alg0210 <abhisheklalgeorge@gmail.com>
2025-05-09 15:10:08 -0400

    initial commit

---
+++ src/test/java/com/WebApp/PokemonList/PokemonListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebApp.PokemonList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
